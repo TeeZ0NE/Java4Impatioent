e61cc607752f17b74fcd8f59c0b5bd04a2fedb5a
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-03-28 17:21:32 +0200

    Recompile into out. Make variable final

--- src/ch1/sec1/FirstSection.java
+++ src/ch1/sec1/FirstSection.java
@@ -3,13 +3,12 @@ package ch1.sec1;
 import java.io.Console;
 
 public class FirstSection {
-    public static void main(String[] args) {
-        Console terminal = System.console();
+    public static void main(final String[] args) {
+        final Console terminal = System.console();
         System.out.println("Yor name: ");
-        String name = terminal.readLine();
-        System.out.print("Age: ");
-        String age = terminal.readLine("User age: ");
-        char[] password = terminal.readPassword("Type password: ");
+        final String name = terminal.readLine();
+        final String age = terminal.readLine("User age: ");
+        final char[] password = terminal.readPassword("Type password: ");
 
         System.out.printf("name: %s, age: %s, passwrd: %s", name, age, new String(password));
     }
--- src/ch1/sec1/WaitQConsole.java
+++ src/ch1/sec1/WaitQConsole.java
@@ -1,14 +1,17 @@
 package ch1.sec1;
+
 import java.io.Console;
+
 public class WaitQConsole {
-    public static void main(String[] args){
-        Console terminal = System.console();
+    public static void main(final String[] args) {
+        final Console terminal = System.console();
         boolean isDone = Boolean.FALSE;
-        String letter = "Q";
+        final String letter = "Q";
         while (!isDone) {
             System.out.printf("Waiting 4 letter %s\n\r", letter);
-            String input = terminal.readLine("Please type letter: ");
-            if(input.equals(letter)) isDone = Boolean.TRUE;
+            final String input = terminal.readLine("Please type letter: ");
+            if (input.equals(letter))
+                isDone = Boolean.TRUE;
         }
     }
 }
