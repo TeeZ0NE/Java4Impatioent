d741571b13496cf46a296f8772d2548e5a668b19
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-04-05 18:40:20 +0300

    Add employee

--- src/ch1/sec1/Employee.java
+++ src/ch1/sec1/Employee.java
@@ -1,4 +1,49 @@
 package ch1.sec1;
 
+import java.util.EmptyStackException;
+
 public class Employee {
+    private double salary;
+    private String name;
+
+    public Employee(String name) {
+        this.salary = 0.0;
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        String validatedMsgOfNewSalary = validateNewSalary(salary);
+        try {
+            if (!validatedMsgOfNewSalary.isEmpty()) throw new Exception(validatedMsgOfNewSalary);
+            this.salary = salary;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String validateNewSalary(double salary) {
+        String msg = "";
+
+        if (salary < 0) msg = "Value cannot be negative or less then zero";
+        if (salary < this.salary) msg = "U trying to setup salary less then it is";
+        return msg;
+    }
+
+    public boolean increaseSalary(double percentage) {
+        boolean isIncreased = Boolean.FALSE;
+
+        if (percentage > 0) {
+          salary *= (1 + percentage / 100);
+          isIncreased = Boolean.TRUE;
+        }
+        return isIncreased;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
--- src/ch1/sec1/FactoryOfEmployees.java
+++ src/ch1/sec1/FactoryOfEmployees.java
@@ -1,8 +1,17 @@
 package ch1.sec1;
 
-public class FactoryOfEmployyies {
+public class FactoryOfEmployees {
     public static void main(String[] args) {
         Employee employee1 = new Employee("Bob");
-        System.out.printf("salary of %s is %f", employee1.getName(), employee1.getSalary());
+        System.out.printf("salary of %s is %.2f%n", employee1.getName(), employee1.getSalary());
+        System.out.println(employee1.increaseSalary(10));
+        System.out.printf("salary of %s is %.2f%n", employee1.getName(), employee1.getSalary());
+        employee1.setSalary(100);
+        System.out.printf("salary of %s is %.2f%n", employee1.getName(), employee1.getSalary());
+        System.out.println(employee1.increaseSalary(10));
+        System.out.printf("salary of %s is %.2f%n", employee1.getName(), employee1.getSalary());
+        employee1.setSalary(100);
+        System.out.println(employee1.increaseSalary(10));
+        System.out.printf("salary of %s is %.2f%n", employee1.getName(), employee1.getSalary());
     }
 }
