69b5014131b0e4382f3da6991422744bb2df28f9
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-04-22 21:02:48 +0300

    Compare strengs

---
+++ src/ch1/sec2/Substringer.java
@@ -0,0 +1,42 @@
+package ch1.sec2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Substringer {
+    private static ArrayList<String> substrings;
+    private static String min, max;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word =  scanner.next();
+        int k = scanner.nextInt();
+        scanner.close();
+        fillSubstrings(word, k);
+        getMin();
+        System.out.format("min:%s max:%s", min, max);
+    }
+
+    private static void fillSubstrings(String word, int k){
+        int i = 0;
+        substrings = new ArrayList<>();
+        while (i < word.length()) {
+            int length = k+i;
+            substrings.add(word.substring(i, length));
+            if (length == word.length()) break;
+            i++;
+        }
+    }
+
+    private static void getMin(){
+        min = substrings.get(0);
+        for(String word : substrings) {
+            if (min.compareTo(word) < 0){
+                max = min;
+                min = word;
+            } else {
+                max = word;
+            }
+        }
+    }
+}
